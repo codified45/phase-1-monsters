49245f118285e5e7a84899b12dc11cb9a48085d5
Jack Lach <jack.lach4@gmail.com>
2023-02-23 09:31:06 -0500

    add deliverables list in comments

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+/*
+
+Deliverables:
+
+1. When the page loads, show the first 50 monsters. 2. Each monster's name, age, and description should be shown.
+
+3. Above your list of monsters, you should have a form to create a new monster. 
+4. You should have fields for name, age, and description, 
+5. and a 'Create Monster Button'. 
+6. When you click the button, the monster should be added to the list and 
+7. saved in the API.
+
+8. At the end of the list of monsters, show a button. 
+9. When clicked, the button should load the next 50 monsters and show them.
+
+*/
