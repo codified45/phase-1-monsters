786b8b4c17779ec2b2f5da12e6ea8a78933bceaa
Jack Lach <jack.lach4@gmail.com>
2023-02-24 12:14:22 -0500

    access monster names from API call

--- js/index.js
+++ js/index.js
@@ -1,10 +1,57 @@
 
+let limitMonstNum = 50;
+let limitMonstNumString = `/?_limit=${limitMonstNum}`;
+let pageOffset = 1;
+let pageOffsetString = `&_page=${pageOffset}`;
+let queryString = limitMonstNumString + pageOffsetString; 
+console.log(queryString);
+let monstersUrl = `http://localhost:3000/monsters${queryString}`;
 
+const init = () => {
+    const backArrow = document.querySelector('#back');
+    const forwardArrow = document.querySelector('#forward');
+    forwardArrow.addEventListener('click', forwardClick);
+    backArrow.addEventListener('click', backwardClick);
+    fetchMonsters();
+
+};
+
+function fetchMonsters(){
+    fetch(monstersUrl)
+        .then(res => res.json())
+            .then(array => {
+                for (const obj of array){
+                console.log(obj.name);
+                let li = document.createElement('li');
+                
+                // let
+                }
+            });
+};
+
+function forwardClick(e){
+    // e.
+    pageOffset = pageOffset + 1;
+    monstersUrl = `http://localhost:3000/monsters/?_limit=50&_page=${pageOffset}`;
+    fetchMonsters();
+};
+
+function backwardClick(e){
+    // e.
+    if (pageOffset > 1){
+    pageOffset = pageOffset - 1;
+    monstersUrl = `http://localhost:3000/monsters/?_limit=50&_page=${pageOffset}`;
+    fetchMonsters();
+    };
+};
+
+document.addEventListener('DOMContentLoaded', init);
 /*
 
 Deliverables:
 
-1. When the page loads, show the first 50 monsters. 2. Each monster's name, age, and description should be shown.
+1. When the page loads, show the first 50 monsters. 
+2. Each monster's name, age, and description should be shown.
 
 3. Above your list of monsters, you should have a form to create a new monster. 
 4. You should have fields for name, age, and description, 
