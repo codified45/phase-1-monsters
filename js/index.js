e0027fd111f1d7d0d762892ec7c01afa2a6f5cee
Jack Lach <jack.lach4@gmail.com>
2023-02-24 13:02:54 -0500

    create working back and forward buttons on bottom of page

--- js/index.js
+++ js/index.js
@@ -11,12 +11,10 @@ const init = () => {
     const forwardArrow = document.querySelector('#forward');
     forwardArrow.addEventListener('click', forwardClick);
     backArrow.addEventListener('click', backwardClick);
+    const monstContainer = document.querySelector('#monster-container');
     fetchMonsters();
 
-};
-
 function fetchMonsters(){
-    const monstContainer = document.querySelector('#monster-container');
 
     fetch(monstersUrl)
         .then(res => res.json())
@@ -40,6 +38,7 @@ function fetchMonsters(){
 function forwardClick(e){
     pageOffset = pageOffset + 1;
     monstersUrl = `http://localhost:3000/monsters/?_limit=50&_page=${pageOffset}`;
+    monstContainer.replaceChildren();
     fetchMonsters();
 };
 
@@ -47,17 +46,21 @@ function backwardClick(e){
     if (pageOffset > 1){
     pageOffset = pageOffset - 1;
     monstersUrl = `http://localhost:3000/monsters/?_limit=50&_page=${pageOffset}`;
+    monstContainer.replaceChildren();
     fetchMonsters();
     };
 };
+
+};
+
 
 document.addEventListener('DOMContentLoaded', init);
 /*
 
 Deliverables:
 
-1. When the page loads, show the first 50 monsters. 
-2. Each monster's name, age, and description should be shown.
+1. When the page loads, show the first 50 monsters. (done)
+2. Each monster's name, age, and description should be shown. (done)
 
 3. Above your list of monsters, you should have a form to create a new monster. 
 4. You should have fields for name, age, and description, 
@@ -65,7 +68,10 @@ Deliverables:
 6. When you click the button, the monster should be added to the list and 
 7. saved in the API.
 
-8. At the end of the list of monsters, show a button. 
-9. When clicked, the button should load the next 50 monsters and show them.
+8. At the end of the list of monsters, show a button. (done)
+9. When clicked, the button should load the next 50 monsters and show them. (done)
+
+Extra: 
+10. There is also a back button, and it only works when the page count is > 1.  
 
 */
